810f75aeceedf4ab65a11fa1420f289218c43c1a
Breno Cunha Queiroz <breno1423@gmail.com>
2025-03-27 11:53:09 +0100

    Feat: Animated text done

--- .github/scripts/animated_text/animated_text.py
+++ .github/scripts/animated_text/animated_text.py
@@ -1,20 +1,56 @@
-TEXT = "Hi there! I'm Breno 👋"
+TEXT = "Hi there! I'm Bruno|<<<endon|<<<no|<<o"
 FILENAME = "brenocq_typing.svg"
 
 def build_svg_animation(text: str) -> str:
-    width = 210
-    height = 30
+    width = 250
+    height = 50
     font_size = 20
+
+    # Build <text> blocks for each step of the typing
+    lines = []
+    visible = ""
+    step = 0
+    time = 0
+    for idx, char in enumerate(text):
+        if char == "|":
+            continue
+        if char == "<":
+            visible = visible[:-1]  # backspace
+        else:
+            visible += char
+
+        # Make sure to freeze the last step
+        is_last_step = (idx == len(text) - 1)
+        fill_type = "freeze" if is_last_step else "remove"
+
+        # Compute delay between chars
+        is_pause = text[idx + 1] == "|" if not is_last_step else False
+        delay = 1.0 if is_pause else 0.15
+
+        lines.append(f"""
+        <text x="0" y="{font_size}" text-anchor="start" opacity="0">
+            <set attributeName="opacity" to="1" begin="{round(time, 3)}s" dur="{delay}" fill="{fill_type}" />
+            {visible}
+        </text>
+        """)
+        step += 1
+        time += delay
+
+    emoji_x = len(visible) * font_size * 0.48;
+    pivot_x = emoji_x + 22
+    pivot_y = font_size
 
     # Create SVG content
     svg = f"""
     <svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">
+        <!-- Made as a joke by brenocq, people get my name wrong all the same at Starbucks -->
         <style>
             text {{
                 font-family: Arial, sans-serif;
                 font-weight: bold;
                 font-size: {font_size}px;
                 fill: #1F2328;
+                opacity: 0;
             }}
             @media (prefers-color-scheme: dark) {{
                 text {{
@@ -22,7 +58,20 @@ def build_svg_animation(text: str) -> str:
                 }}
             }}
         </style>
-        <text x="0" y="{font_size}" text-anchor="start">{text}</text>
+
+        <!-- Typing text -->
+        {''.join(lines)}
+
+        <!-- Animated emoji -->
+        <text x="{emoji_x}" y="{font_size}" text-anchor="start" opacity="0">
+            <set attributeName="opacity" to="1" begin="{round(time+2*delay, 3)}s" dur="0.001s" fill="freeze" />
+            👋
+            <animateTransform attributeName="transform"
+                type="rotate"
+                values="-20 {pivot_x} {pivot_y}; 20 {pivot_x} {pivot_y}; -20 {pivot_x} {pivot_y}"
+                dur="0.5s"
+                repeatCount="indefinite" />
+        </text>
     </svg>
     """
     return svg
